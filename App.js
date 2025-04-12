9cb05d5d29ce87347a0ceb6fa57e98d298b18786
RussoPy <galrusso3@gmail.com>
2025-04-12 13:13:10 +0300

    status bar

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import AppNavigator from './src/navigation/AppNavigator';
+import { StatusBar } from 'expo-status-bar';
 
 export default function App() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
+      <StatusBar style="light" backgroundColor="#18181b" />
       <AppNavigator />
     </GestureHandlerRootView>
   );
