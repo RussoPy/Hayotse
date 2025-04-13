3f49cc0b3f88051a70d54bd8e3ee4b6d43506f7d
RussoPy <galrusso3@gmail.com>
2025-04-13 13:38:53 +0300

    left to right

--- App.js
+++ App.js
@@ -1,8 +1,16 @@
+import { useEffect } from 'react';
+import { I18nManager } from 'react-native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import AppNavigator from './src/navigation/AppNavigator';
 import { StatusBar } from 'expo-status-bar';
+import AppNavigator from './src/navigation/AppNavigator';
 
 export default function App() {
+  useEffect(() => {
+    // Force LTR layout
+    I18nManager.allowRTL(false);
+    I18nManager.forceRTL(false);
+  }, []);
+
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <StatusBar style="light" backgroundColor="#18181b" />
